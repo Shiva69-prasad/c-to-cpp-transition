5fe996f00e6f38f8cd99af0ce726132f562781a2
Shiva69-prasad <shiva31694@gmail.com>
2025-08-28 07:57:44 +0530

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,39 @@
+  #include<iostream>
+  int main(){
+      std::cout <<"hello world\n";
+      std::cout <<"shivaprasad";
+
+      return 0;
+  }
+
+  #include<iostream>
+  using namespace std;
+  int main(){
+      cout <<"hlo world" << endl;// using namespace std;
+
+      cout <<"shivaprasad";
+
+      return 0;
+  }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b; //sum
+    cout <<"enter the value of a" <<endl;
+    cin >>a;
+
+    cout <<"enter the value of b" <<endl;
+    cin >>b;
+
+    // sum=a+b;
+    
+    // cout<<"the sum is" <<a <<"&" <<b <<"is:" <<sum <<endl;
+
+    // cout<<"the sum is" <<a <<"&" <<b <<"is:" <<a+b <<endl;
+
+    cout <<a <<"+" <<b <<"=" <<a+b <<endl;
+
+
+    return 0;
+}
