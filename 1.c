543c9ec9e271bc937d77044bff6ed597d94735ef
Shiva69-prasad <shiva31694@gmail.com>
2025-08-28 07:59:15 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,22 @@
+  #include<stdio.h>
+  void main(){
+      printf("hello world");
+  }
+
+#include<stdio.h>
+
+int main(){
+    int a,b; //sum
+    printf("enter a:\n");
+    scanf("%d",&a);
+
+    printf("eneter b:\n");
+    scanf("%d",&b);
+
+    // sum=a+b;
+
+    // printf("the sum of %d & %d is:%d\n",a,b,sum);
+   
+    printf("the sum of %d + %d =%d\n",a,b,a+b);
+
+}
